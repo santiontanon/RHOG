043166f5e68176e434322e74cbd9723a72ec0513
santiontanon <santi.ontanon@gmail.com>
2016-09-01 21:42:40 +0200

    added the singleAntiunificationWithMappings function in the AntiUnification class, which returns the mappings through which the antiunifier subsumes the original terms

--- src/dlg/core/operations/AntiUnification.java
+++ src/dlg/core/operations/AntiUnification.java
@@ -20,6 +20,20 @@ import dlg.util.Pair;
 public class AntiUnification {
     public static int DEBUG = 0;
 
+    
+    public static class AntiUnificationResult {
+        public DLG m_antiunifier = null;
+        public int m_steps;
+        public List<int[]> m_mappings;
+        
+        public AntiUnificationResult(DLG a_antiunifier, int a_steps, List<int[]> a_mappings) {
+            m_antiunifier = a_antiunifier;
+            m_steps = a_steps;
+            m_mappings = a_mappings;
+        }
+    }
+    
+    
     public static DLG singleAntiunification(List<DLG> gs, Subsumption s, RefinementOperator rho) throws Exception {
         return singleAntiunificationSteps(gs, s, rho).m_a;
     }    
@@ -40,13 +54,21 @@ public class AntiUnification {
         return singleAntiunificationSteps(l, s, rho);
     }
    
-   
+
     public static Pair<DLG,Integer> singleAntiunificationSteps(List<DLG> gs, Subsumption s, RefinementOperator rho) throws Exception {
+        AntiUnificationResult result = singleAntiunificationWithMappings(gs, s, rho);
+        return new Pair<DLG,Integer>(result.m_antiunifier, result.m_steps);
+    }    
+    
+    
+    public static AntiUnificationResult singleAntiunificationWithMappings(List<DLG> gs, Subsumption s, RefinementOperator rho) throws Exception {
         DLG au = rho.getTop();
         int steps = 0;
+        List<int []> mappings = null;
         
         do {
             DLG next_au = null;
+            List<int []> next_mappings = null;
             if (rho instanceof IncrementalRefinementOperator) {
                 // if we have an incremental operator, exploit it!
                 IncrementalRefinementOperator irho = (IncrementalRefinementOperator)rho;
@@ -56,15 +78,19 @@ public class AntiUnification {
                     DLG candidate_au = irho.getNextDownwardRefinement();
                     if (candidate_au == null) break;
                     nrefinements_generated++;
-                    boolean subsumeAll = true;
+                    List<int []> candidate_mappings = new ArrayList<>();
                     for(DLG g:gs) {
-                        if (s.subsumes(candidate_au, g)==null) {
-                            subsumeAll = false;
+                        int []m = s.subsumes(candidate_au, g);
+                        if (m==null) {
+                            candidate_mappings = null;
                             break;
+                        } else {
+                            candidate_mappings.add(m);
                         }
                     }
-                    if (subsumeAll) {
+                    if (candidate_mappings!=null) {
                         next_au = candidate_au;
+                        next_mappings = candidate_mappings;
                         steps++;
                         break;
                     }
@@ -73,23 +99,28 @@ public class AntiUnification {
             } else {
                 List<? extends DLG> refinements = rho.downwardRefinements(au);
                 if (DEBUG>=1) System.out.println("AntiUnification.singleAntiunification: refinements " + refinements.size());
+                List<int []> candidate_mappings = new ArrayList<>();
                 for(DLG candidate_au: refinements) {
-                    boolean subsumeAll = true;
                     for(DLG g:gs) {
-                        if (s.subsumes(candidate_au, g)==null) {
-                            subsumeAll = false;
+                        int []m = s.subsumes(candidate_au, g);
+                        if (m==null) {
+                            candidate_mappings = null;
                             break;
+                        } else {
+                            candidate_mappings.add(m);
                         }
                     }
-                    if (subsumeAll) {
+                    if (candidate_mappings!=null) {
                         next_au = candidate_au;
+                        next_mappings = candidate_mappings;
                         steps++;
                         break;
                     }
                 }
             }
-            if (next_au == null) return new Pair<>(au, steps);
+            if (next_au == null) return new AntiUnificationResult(au, steps, mappings);
             au = next_au;
+            mappings = next_mappings;
             if (DEBUG>=2) {
                 System.out.println("AntiUnification.singleAntiunification: current antiunification:");
                 System.out.println(au);
